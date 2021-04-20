3eedc24cce6d1502276515970afdbd62334d489e
Maxime <maxime.vanrechem@student.vinci.be>
2021-04-20 16:38:41 +0200

    3307

--- models/Db.class.php
+++ models/Db.class.php
@@ -7,7 +7,8 @@ class Db
     private function __construct()
     {
         try {
-            $this->_connection = new PDO('mysql:host=localhost;port=3306;dbname=webproject;charset=utf8','root','');
+            //$this->_connection = new PDO('mysql:host=localhost;port=3306;dbname=webproject;charset=utf8','root','');
+            $this->_connection = new PDO('mysql:host=localhost;port=3307;dbname=webproject;charset=utf8','root','');
             $this->_connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 			$this->_connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
         } 
